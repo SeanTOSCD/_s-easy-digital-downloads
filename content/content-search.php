032fedd8718595fec2af1693ea1eb38613882028
Sean Davis <sdavis2702@gmail.com>
2014-02-18 03:58:46 -0600

    add search results template

---
+++ content/content-search.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * search results display
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<div class="entry-meta">
+			<?php presentation_posted_on(); ?>
+		</div>
+	</header>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div>
+</article>
