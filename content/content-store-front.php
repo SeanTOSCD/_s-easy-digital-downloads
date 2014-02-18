5fb487b17736ec6ee00f2456b8e586f48d2a1cc1
Sean Davis <sdavis2702@gmail.com>
2014-02-18 04:26:32 -0600

    set default store front quantity

--- content/content-store-front.php
+++ content/content-store-front.php
@@ -6,7 +6,7 @@
  */
 $store_page_setting = (is_front_page() && is_page_template('edd_templates/edd-store-front.php') ? 'page' : 'paged' );
 $current_page = get_query_var( $store_page_setting );
-$per_page = intval( get_theme_mod( 'sdm_store_front_count' ) );
+$per_page = intval( get_theme_mod( 'sdm_store_front_count', 9 ) );
 $offset = $current_page > 0 ? $per_page * ( $current_page-1 ) : 0;
 $product_args = array(
 	'post_type'			=> 'download',
