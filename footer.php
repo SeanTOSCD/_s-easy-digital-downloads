9cb2d00e7349e2f4678ea13fc2a785a3236e5e4b
Sean Davis <sdavis2702@gmail.com>
2014-04-20 17:31:05 -0500

    add site name to footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 		<div class="main">
 			<footer id="colophon" class="site-footer inner" role="contentinfo">
 				<span class="site-info">
-					<?php echo get_theme_mod( 'sdm_credits_copyright', '&copy; ' . date( 'Y' ) ); ?>
+					<?php echo get_theme_mod( 'shoppette_credits_copyright', get_bloginfo( 'description' ) . ' &copy; ' . date( 'Y' ) ); ?>
 				</span>
 			</footer>
 		</div>
