7d664c1da296014618a52adef241e01cdc42fa45
Sean Davis <sdavis2702@gmail.com>
2014-04-27 17:15:19 -0500

    added new default search form with relevant markup

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * default search form
+ */
+?>
+<form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <div class="search-wrap">
+    	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'presentation' ); ?></label>
+        <input type="search" placeholder="<?php echo esc_attr( 'Search&hellip;', 'presentation' ); ?>" name="s" id="search-input" />
+        <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
+    </div>
+</form>
