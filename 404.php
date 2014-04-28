722a80ac29e07785ef7f58ea141cdc8fa6742235
Sean Davis <sdavis2702@gmail.com>
2014-04-28 11:51:07 -0500

    change 404 copy to something that makes sense!

--- 404.php
+++ 404.php
@@ -9,12 +9,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'sdm' ); ?></h1>
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'sdm' ); ?></h1>
 				</header>
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'sdm' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Try using the search form below.', 'sdm' ); ?></p>
 
 					<?php get_search_form(); ?>
 				</div>
