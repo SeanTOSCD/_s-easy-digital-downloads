c1bdbccf3728e51593fae8b821bc1bf813303cc2
Sean Davis <sdavis2702@gmail.com>
2014-04-28 11:52:12 -0500

    add query string to search form value attr

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
 <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="search-wrap">
     	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'presentation' ); ?></label>
-        <input type="search" placeholder="<?php echo esc_attr( 'Search&hellip;', 'presentation' ); ?>" name="s" id="search-input" />
+        <input type="search" placeholder="<?php echo esc_attr( 'Search&hellip;', 'presentation' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
         <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
     </div>
 </form>
