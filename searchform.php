74d369d4181283b8496a5c447f6822f9b671ddfb
Sean Davis <sdavis2702@gmail.com>
2014-06-19 14:18:11 -0500

    fix incorrect text domain

--- searchform.php
+++ searchform.php
@@ -5,8 +5,8 @@
 ?>
 <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="search-wrap">
-    	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'presentation' ); ?></label>
-        <input type="search" placeholder="<?php esc_attr_e( 'Search&hellip;', 'presentation' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
+    	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'sdm' ); ?></label>
+        <input type="search" placeholder="<?php esc_attr_e( 'Search&hellip;', 'sdm' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
         <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
     </div>
 </form>
