48c0bea4a0c0b4f9df7e1790ed7980614013b78c
Sean Davis <sdavis2702@gmail.com>
2014-08-21 11:43:52 -0500

    fix download taxonomy pagination

--- content/content-download-taxonomy.php
+++ content/content-download-taxonomy.php
@@ -55,7 +55,7 @@
 			echo paginate_links( array(
 				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 				'format' => '?paged=%#%',
-				'current' => max( 1, $current_page ),
+				'current' => max( 1, get_query_var( 'paged' ) ),
 				'total' => $wp_query->max_num_pages
 			) );
 		?>
